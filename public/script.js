818c470e820f4c65af393d26a482769714ab23bd
yuvraj-9732 <yuvraj.parekh9732@gmail.com>
2024-07-08 19:21:10 +0530

    MY FIRST COMMIT

---
+++ public/script.js
@@ -0,0 +1,43 @@
+const socket = io("/");
+
+
+
+const user = prompt("Enter your name");
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
+
+
+
+socket.on("createMessage", (message) => {
+    $(".messages").append(`
+        <div class="message">
+            <span>${message}</span>
+        </div>
+    `)
+});
